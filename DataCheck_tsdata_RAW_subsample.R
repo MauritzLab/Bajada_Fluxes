6448347c105c4516d8efd6aeaeb1a3b4610b4418
MauritzLab <memauritz@utep.edu>
2026-04-15 16:32:35 -0600

    data check april 15

--- DataCheck_tsdata_RAW_subsample.R
+++ DataCheck_tsdata_RAW_subsample.R
@@ -32,7 +32,7 @@ ts.all <- do.call("rbind", lapply(ts_files, header = FALSE, fread, sep=",", skip
 max(ts.all$TIMESTAMP)
 
 # MODIFY AS NEEDED: subset the data to include only the last month (or since desired date)
-ts <- copy(ts.all[TIMESTAMP>as.Date("2026-03-01")])
+ts <- copy(ts.all[TIMESTAMP>as.Date("2026-04-01")])
 
 # CO2 concentrations
 # Check: how well do patterns match between open and closed path
